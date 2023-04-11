e96971f9aa03342fb75e7fbbfdad11abc9b85ba0
Antonio Marcelo <antoniomarcelo545@gmail.com>
2023-04-11 10:34:35 -0300

    AtvFinalizada

--- loteria/src/components/home/Home.tsx
+++ loteria/src/components/home/Home.tsx
@@ -10,52 +10,73 @@ import Titulo from '../titulo/titulo';
 import TituloLoto from '../titulo/tituloLoto';
 import Valor from '../valor/valor';
 import ValorLoto from '../valor/valorLoto';
+
 export default function Megasena() {
 
-  const {megasena} = useContexto()
-  const {lotofacil} = useContexto()
+  const { megasena } = useContexto()
+  const { lotofacil } = useContexto()
+
+  function verificaMega() {
+
+    if (megasena.quantidadeGanhadores === 1) {
+      return `${megasena.quantidadeGanhadores} GANHADOR`
+    } if (megasena.quantidadeGanhadores === 0) {
+      return 'ACUMULADO'
+    } else {
+      return `${megasena.quantidadeGanhadores} GANHADOR`
+    }
+  }
+
+  function verificaLoto() {
+
+    if (lotofacil.quantidadeGanhadores === 1) {
+      return `${lotofacil.quantidadeGanhadores} GANHADOR`
+    } if (lotofacil.quantidadeGanhadores === 0) {
+      return 'ACUMULADO'
+    } else {
+      return `${lotofacil.quantidadeGanhadores} GANHADOR`
+    }
+  }
 
   return (
     <div>
-    <div className='cont'>
+      <div className='cont'>
 
-      <div className='direito'>
+        <div className='direito'>
 
-        <div className='trevo'><img src={trevoMega} /><Titulo titulo={"Mega-sena"} /> </div>
-        <ProximoSorteio informacoes={"Estimativa de prêmio do proximo concurso. "} data={`Sorteio em ${megasena.dataProximoConcurso}:`} />
-        <Valor valor={megasena.valorEstimadoProximoConcurso} />
+          <div className='trevo'><img src={trevoMega} /><Titulo titulo={"Mega-sena"} /> </div>
+          <ProximoSorteio informacoes={"Estimativa de prêmio do proximo concurso. "} data={`Sorteio em ${megasena.dataProximoConcurso}:`} />
+          <Valor valor={megasena.valorEstimadoProximoConcurso.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} />
+
+        </div>
+
+        <div className='esquerdo'>
+
+          <NumerosSorteados numeros={megasena.dezenas} />
+          <Ganhadores ganhadores={verificaMega()} />
+          <InforConcurso informacoes={`Concurso: ${megasena.numeroDoConcurso} -`} data={` ${megasena.dataPorExtenso}`} />
+
+        </div>
 
       </div>
 
-      <div className='esquerdo'>
+      <div className='cont'>
 
-        <NumerosSorteados numeros={megasena.dezenas} />
-        <Ganhadores ganhadores={megasena.quantidadeGanhadores ? `${megasena.quantidadeGanhadores} GANHADORES` : "ACUMULOU"} />
-        <InforConcurso informacoes={`Concuso: ${megasena.numeroDoConcurso} -`} data={` ${megasena.dataPorExtenso}`} />
+        <div className='direito'>
+
+          <div className='trevo'><img src={trevoLoto} /><TituloLoto titulo={"Loto facil"} /></div>
+          <ProximoSorteio informacoes={"Estimativa de prêmio do proximo concurso. "} data={`Sorteio em ${lotofacil.dataProximoConcurso}:`} />
+          <ValorLoto valor={lotofacil.valorEstimadoProximoConcurso.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} />
+
+        </div>
+        <div className='esquerdo'>
+          <NumerosSorteadosLoto numeros={lotofacil.dezenas} />
+          <Ganhadores ganhadores={verificaLoto()} />
+          <InforConcurso informacoes={`Concurso: ${lotofacil.numeroDoConcurso} -`} data={` ${lotofacil.dataPorExtenso}`} />
+
+        </div>
 
       </div>
-
     </div>
-
-    <div className='cont'>
-
-      <div className='direito'>
-
-        <div className='trevo'><img src={trevoLoto} /><TituloLoto titulo={"Loto facil"} /></div>
-        <ProximoSorteio informacoes={"Estimativa de prêmio do proximo concurso. "} data={`Sorteio em ${lotofacil.dataProximoConcurso}:`} />
-        <ValorLoto valor={lotofacil.valorEstimadoProximoConcurso} />
-
-      </div>
-      <div className='esquerdo'>
-
-        <NumerosSorteadosLoto numeros={lotofacil.dezenas} />
-        <Ganhadores ganhadores={lotofacil.quantidadeGanhadores ? `${lotofacil.quantidadeGanhadores} GANHADORES` : "ACUMULOU"} />
-        <InforConcurso informacoes={`Concuso: ${lotofacil.numeroDoConcurso} -`} data={` ${lotofacil.dataPorExtenso}`} />
-
-      </div>
-
-    </div>
-  </div>
-
   )
 }
--- loteria/src/pages/Principal.tsx
+++ loteria/src/pages/Principal.tsx
@@ -2,15 +2,19 @@ import Home from "../components/home/Home";
 import { useContexto } from "../hooks";
 
 export default function Principal() {
-  const {megasena} = useContexto()
+  const { megasena } = useContexto()
   return (
     <>
-      {megasena.numeroDoConcurso ?
-        <div>
-          <Home />
-        </div>
-        :
-        <div>Carregando....</div>}
+      {
+        megasena.numeroDoConcurso ?
+          <div>
+            <Home />
+          </div>
+          :
+          <div className="carregando">
+            <h1>Loading....</h1>
+          </div>
+      }
 
     </>
   )
